8eaee7b2ae47ca19667b82aab2e78b908bc0a7b2
Judson Ciribelli <judson.ciribelli17@gmail.com>
2025-08-15 12:58:56 -0300

    feat: add zod schema validation

--- server.ts
+++ server.ts
@@ -1,8 +1,16 @@
 import fastify from "fastify";
-import crypto from "node:crypto";
 import { db } from "./src/database/client.ts";
+import { fastifySwagger } from "@fastify/swagger";
+import { fastifySwaggerUi } from "@fastify/swagger-ui";
 import { courses } from "./src/database/schema.ts";
-import { eq } from "drizzle-orm";
+import {
+  validatorCompiler,
+  serializerCompiler,
+  type ZodTypeProvider,
+  jsonSchemaTransform,
+} from "fastify-type-provider-zod";
+import { desc, eq } from "drizzle-orm";
+import z from "zod";
 
 const server = fastify({
   logger: {
@@ -14,6 +22,23 @@ const server = fastify({
       },
     },
   },
+}).withTypeProvider<ZodTypeProvider>();
+
+server.setValidatorCompiler(validatorCompiler);
+server.setSerializerCompiler(serializerCompiler);
+
+server.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "Class Manager API",
+      version: "1.0.0",
+    },
+  },
+  transform: jsonSchemaTransform,
+});
+
+server.register(fastifySwaggerUi, {
+  routePrefix: "/docs",
 });
 
 server.get("/courses", async (request, reply) => {
@@ -21,106 +46,122 @@ server.get("/courses", async (request, reply) => {
   return reply.send({ courses: result });
 });
 
-server.get("/courses/:id", async (request, reply) => {
-  type Params = {
-    id: string;
-  };
-  const params = request.params as Params;
-  const id = params.id;
-  const result = await db.select().from(courses).where(eq(courses.id, id));
+server.get(
+  "/courses/:id",
+  {
+    schema: {
+      params: z.object({
+        id: z.uuid(),
+      }),
+    },
+  },
+  async (request, reply) => {
+    const params = request.params;
+    const id = params.id;
+    const result = await db.select().from(courses).where(eq(courses.id, id));
 
-  if (result.length > 0) {
-    return { course: result[0] };
-  }
+    if (result.length > 0) {
+      return { course: result[0] };
+    }
 
-  return reply.status(404).send({ message: "Course not found" });
-});
-
-server.post("/courses", async (request, reply) => {
-  type CourseBody = {
-    title: string;
-    description: string;
-  };
-
-  const body = request.body as CourseBody;
-  const courseTitle = body.title;
-  const coruseDescription = body.description;
-
-  if (!courseTitle) {
-    return reply.status(400).send({ message: "Course title is required" });
-  }
-
-  if (!coruseDescription) {
-    return reply
-      .status(400)
-      .send({ message: "Course Description is required" });
-  }
-
-  const result = await db
-    .insert(courses)
-    .values({
-      title: courseTitle,
-      description: coruseDescription,
-    })
-    .returning();
-
-  return reply.status(201).send({ course: result });
-});
-
-server.delete("/courses/:id", async (request, reply) => {
-  type Params = {
-    id: string;
-  };
-
-  const params = request.params as Params;
-  const id = params.id;
-  const result = await db.select().from(courses).where(eq(courses.id, id));
-
-  if (!result) {
-    return reply.status(404).send({ message: "Course not foun" });
-  }
-  await db.delete(courses).where(eq(courses.id, id));
-
-  return reply.status(201).send({ message: "Course deleted" });
-});
-
-server.patch("/courses/:id", async (request, reply) => {
-  type Params = {
-    id: string;
-    title?: string;
-    description?: string;
-  };
-
-  const params = request.params as Params;
-  const body = request.body as Params;
-  const courseTitle = body.title;
-  const courseDescription = body.description;
-  const id = params.id;
-
-  const course = await db.select().from(courses).where(eq(courses.id, id));
-
-  if (!course) {
     return reply.status(404).send({ message: "Course not found" });
   }
+);
 
-  if (courseTitle) {
-    course[0].title = courseTitle;
+server.post(
+  "/courses",
+  {
+    schema: {
+      body: z.object({
+        title: z.string(),
+        description: z.string(),
+      }),
+    },
+  },
+  async (request, reply) => {
+    const body = request.body;
+    const courseTitle = body.title;
+    const coruseDescription = body.description;
+
+    const result = await db
+      .insert(courses)
+      .values({
+        title: courseTitle,
+        description: coruseDescription,
+      })
+      .returning();
+
+    return reply.status(201).send({ course: result });
   }
+);
 
-  if (courseDescription) {
-    course[0].description = courseDescription;
+server.delete(
+  "/courses/:id",
+  {
+    schema: {
+      params: z.object({
+        id: z.uuid(),
+      }),
+    },
+  },
+  async (request, reply) => {
+    const params = request.params;
+    const id = params.id;
+    const result = await db.select().from(courses).where(eq(courses.id, id));
+
+    if (!result) {
+      return reply.status(404).send({ message: "Course not foun" });
+    }
+    await db.delete(courses).where(eq(courses.id, id));
+
+    return reply.status(201).send({ message: "Course deleted" });
   }
+);
 
-  await db
-    .update(courses)
-    .set({
-      title: course[0].title,
-      description: course[0].description,
-    })
-    .where(eq(courses.id, id));
+server.patch(
+  "/courses/:id",
+  {
+    schema: {
+      params: z.object({
+        id: z.uuid(),
+        title: z
+          .string()
+          .nonempty()
+          .nonempty({ message: "Title cannot be empty" }),
+        description: z
+          .string()
+          .nonempty({ message: "Description cannot be empty" }),
+      }),
+    },
+  },
+  async (request, reply) => {
+    const params = request.params;
+    const body = request.body;
+    const courseTitle = params.title;
+    const courseDescription = params.description;
+    const id = params.id;
 
-  return reply.status(201).send({ courses });
-});
+    const course = await db.select().from(courses).where(eq(courses.id, id));
+
+    if (courseTitle) {
+      course[0].title = courseTitle;
+    }
+
+    if (courseDescription) {
+      course[0].description = courseDescription;
+    }
+
+    await db
+      .update(courses)
+      .set({
+        title: course[0].title,
+        description: course[0].description,
+      })
+      .where(eq(courses.id, id));
+
+    return reply.status(201).send({ courses });
+  }
+);
 
 server.listen({ port: 3000 }).then(() => {
   console.log("Server listening on http://localhost:3000");
