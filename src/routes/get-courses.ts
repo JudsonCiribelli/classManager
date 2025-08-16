6f84403bd4bf8dccbcdff464b713888c877aeec9
Judson Ciribelli <judson.ciribelli17@gmail.com>
2025-08-16 19:32:02 -0300

    feat: add search params in getCourse route

--- src/routes/get-courses.ts
+++ src/routes/get-courses.ts
@@ -2,6 +2,7 @@ import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
 import { db } from "../database/client.ts";
 import { courses } from "../database/schema.ts";
 import z from "zod";
+import { and, asc, ilike, SQL } from "drizzle-orm";
 
 export const getCourseRoute: FastifyPluginAsyncZod = async (server) => {
   server.get(
@@ -10,6 +11,11 @@ export const getCourseRoute: FastifyPluginAsyncZod = async (server) => {
       schema: {
         tags: ["Courses"],
         summary: "Get all courses",
+        querystring: z.object({
+          search: z.string().optional(),
+          orderBy: z.enum(["title", "id"]).optional().default("id"),
+          page: z.coerce.number().optional().default(1),
+        }),
         response: {
           200: z.object({
             courses: z.array(
@@ -19,20 +25,36 @@ export const getCourseRoute: FastifyPluginAsyncZod = async (server) => {
                 description: z.string(),
               })
             ),
+            total: z.number(),
           }),
         },
       },
     },
     async (request, reply) => {
-      const result = await db
-        .select({
-          id: courses.id,
-          title: courses.title,
-          description: courses.description,
-        })
-        .from(courses);
+      const { search, orderBy, page } = request.query;
 
-      return reply.send({ courses: result });
+      const conditions: SQL[] = [];
+
+      if (search) {
+        conditions.push(ilike(courses.title, `%${search}%`));
+      }
+
+      const [result, total] = await Promise.all([
+        db
+          .select({
+            id: courses.id,
+            title: courses.title,
+            description: courses.description,
+          })
+          .from(courses)
+          .orderBy(asc(courses[orderBy]))
+          .offset((page - 1) * 2)
+          .limit(2)
+          .where(and(...conditions)),
+        db.$count(courses, and(...conditions)),
+      ]);
+
+      return reply.send({ courses: result, total });
     }
   );
 };
